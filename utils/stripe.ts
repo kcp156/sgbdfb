1017cc86e5ffb83f992e0c7264f9ff30b1b6ffc3
khanchitpon <136857718+kcp156@users.noreply.github.com>
2023-10-09 13:51:23 +0700

    Update stripe.ts

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 
 export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY ?? '',
-  {
+  
     // https://github.com/stripe/stripe-node#configuration
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
@@ -10,5 +10,5 @@ export const stripe = new Stripe(
       name: 'Next.js Subscription Starter',
       version: '0.1.0'
     }
-  }
+  
 );
