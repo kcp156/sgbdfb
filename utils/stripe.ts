8887981559a29fd25e56c6f35f247c936c5d8a8f
khanchitpon <136857718+kcp156@users.noreply.github.com>
2023-10-09 13:46:48 +0700

    Update stripe.ts

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -4,7 +4,6 @@ export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY ?? '',
   {
     // https://github.com/stripe/stripe-node#configuration
-    apiVersion: '2022-11-15',
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
